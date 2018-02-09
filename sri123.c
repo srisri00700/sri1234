75eb04137f6bdd01d1a283d613424444b4d87cad
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-02-09 15:38:43 +0530

    sri123.c

---
+++ sri123.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int m,h;
+    scanf("%d",&m);
+    if(m>=60)
+    {
+        h=m/60;
+        m=m%60;
+    
+    printf("%d %d",h,m);
+    }
+    else
+    printf("0 %d",m);
+}
